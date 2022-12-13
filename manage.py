05c97ee0b6e0460980bafa4666976f9c29f70236
Ravi <muhammadravi251001@gmail.com>
2022-12-13 10:41:35 +0700

    fixing name

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DataFlair_discsnForum.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mini_Socmed.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
