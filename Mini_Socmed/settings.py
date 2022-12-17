4dc3399f16c16b147cf3502936d3146db6701d45
Ravi <muhammadravi251001@gmail.com>
2022-12-17 21:12:06 +0700

    change databases and add pymysql

--- Mini_Socmed/settings.py
+++ Mini_Socmed/settings.py
@@ -74,13 +74,25 @@ WSGI_APPLICATION = 'Mini_Socmed.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_mini_socmed',
+        'HOST': 'database-1.cdvrlsstpnjm.us-east-1.rds.amazonaws.com',
+        'USER': 'admin',
+        'PASSWORD': 'adminadmin',
+        'PORT': '3306'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
